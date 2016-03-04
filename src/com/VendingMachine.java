c0389766e2b240602d5306a03a920ba8a6af7d2d
sabatinipeter <sabatinipeter@gmail.com>
2016-03-03 22:12:55 -0500

    Update test to cover cases of accepting nickels, dime, quarter but not pennies

--- src/com/VendingMachine.java
+++ src/com/VendingMachine.java
@@ -3,7 +3,7 @@ package com;
 public class VendingMachine {
 
 	public boolean accept(int coin) {
-		if(coin == 5) {
+		if(coin == 5 || coin == 10 || coin == 25) {
 			return true;
 		} else {
 			return false;	
--- src/test/VendingMachineTest.java
+++ src/test/VendingMachineTest.java
@@ -8,11 +8,20 @@ import com.VendingMachine;
 
 public class VendingMachineTest {
 
+	private static final int PENNIE = 1;
 	private static final int NICKEL = 5;
+	private static final int DIME = 10;
+	private static final int QUARTER = 25;
 
 	@Test
 	public void vendingMachineAcceptsANickel() {
 		VendingMachine vm = new VendingMachine();
 		assertEquals(true, vm.accept(NICKEL));
+		assertEquals(true, vm.accept(DIME));
+		assertEquals(true, vm.accept(QUARTER));
+		assertEquals(false, vm.accept(PENNIE));
+		
 	}
+	
+	
 }
